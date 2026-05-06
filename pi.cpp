403150bb45ad87a90be2fa341c716f71656a3e13
unknown <anh.hung.ta.2007@gmail.com>
2026-05-06 21:22:47 +0900

    Calculating Pi Homework

---
+++ pi.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    double x = 0;
+    int i = 0;
+    while (i < 1000000){
+        if (i%2==0) {
+            x = x + 1.0/(2*i+1);
+        }
+        else {
+            x = x - 1.0/(2*i+1);
+        }
+        ++i;
+    }
+    x = x * 4;
+    cout<<setprecision(7)<<x;
+    return 0;
+}
